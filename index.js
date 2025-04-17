0b0138364c2a46e9df2cf8add6a69f8b996dbc1a
Aashi-code77 <pandaoutlier@gmail.com>
2025-04-17 12:06:18 +0000

    fun test app for backend

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.get('/twitter', (req, res) => {
+  res.send('CoderAashi')
+})
+
+app.get('/login',(req, res) => {
+  res.send('<h1>please login at CoderAashi</h1>')
+})
+
+app.get('/youtube',(req,res) =>{
+  res.send("<h2>CoderAashi</h2>")
+})
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
